6a3673a7326d727185667b8b47849cf429dfad16
Jack-Grogan <Jackrgrogan@hotmail.com>
2025-04-01 12:11:59 +0100

    Implementation of homogeneity index

--- DEMToolbox/mixing/homogeneity_index.py
+++ DEMToolbox/mixing/homogeneity_index.py
@@ -2,38 +2,131 @@ import numpy as np
 import warnings
 import pandas as pd
 
-def homogeneity_index(particle_data, attribute_column, 
-                      sample_column, homogeniety_column=None, verbose=False):
-    
-    """ Calculate the homogeneity index of the particle data.
-    """
+from ..classes.particle_samples import ParticleSamples
 
+def homogeneity_index(particle_data, attribute_column, 
+                      samples:ParticleSamples, homogeniety_column=None, 
+                      verbose=False):
+    """ Calculate the homogeneity index of the particle data.
+
+    The homogeneity index is a measure of the uniformity of a property
+    across a sample of particles. It is calculated as the standard
+    deviation of the sample mean of the property in each mesh element,
+    with respect to the bulk mean of the property as defined in the 
+    work of Windows-Yule et al. [1].
+
+    References
+    ----------
+
+    [1] Windows-Yule K, Nicuşan L, Herald MT, Manger S, Parker D. 
+        Positron emission particle tracking: A comprehensive guide. 
+        IOP Publishing; 2022 Jun 1.
+
+    Parameters
+    ----------    
+    
+    particle_data : vtkPolyData
+        The particle vtk.
+    attribute_column : str
+        The name of the attribute column whos deviation across the
+        samples is to be calculated.
+    samples : ParticleSamples
+        The samples object containing the sample data to calculate the
+        homogeneity index for.
+    homogeniety_column : str, optional
+        The name of the column to store the sample mean values.
+        If None, the default is to use the attribute column name with
+        "_homogeneity" appended to it.
+    verbose : bool, optional
+        Print the homogeneity index, bulk mean, sample means, and
+        number of samples, by default False.
+
+    Returns
+    -------
+    particle_data : vtkPolyData
+        The particle vtk with the homogeneity column added.
+    homogeneity_index : float
+        The homogeneity index of the particle data.
+
+    Raises
+    ------
+    UserWarning
+        If the particle data has no points return unedited particle
+        data and NaN for homogeneity index.
+    UserWarning
+        If the attribute column is not found in the particle data
+        return unedited particle data and NaN for homogeneity index.
+    UserWarning
+        If the attribute column contains NaN values, the mean
+        will be NaN and a warning will be raised.
+    UserWarning
+        If the sample column is not found in the particle data
+        return unedited particle data and NaN for homogeneity index.
+    ValueError
+        If the attribute column is not numeric.
+    ValueError
+        If the sample column is not numeric.
+    """
+    if particle_data.n_points == 0:
+        warnings.warn(("Cannot calculate homogeneity index for empty "
+                       "particle file."), UserWarning)
+        return particle_data, np.nan
+    
+    if attribute_column not in particle_data.point_data.keys():
+        warnings.warn((f"{attribute_column} not found in particle file, "
+                       "returning NaN."), UserWarning)
+        return particle_data, np.nan
+    
+    if samples.name not in particle_data.point_data.keys():
+        warnings.warn((f"{samples.name} not found in particle file, "
+                       "returning NaN."), UserWarning)
+        return particle_data, np.nan
+    
+    # Check if the attribute column is numeric
+    if not np.issubdtype(particle_data[attribute_column].dtype, np.number):
+        raise ValueError(f"Attribute column {attribute_column} is not numeric.")
+    
+    # Check if the sample column is numeric
+    if not np.issubdtype(particle_data[samples.name].dtype, np.number):
+        raise ValueError(f"Sample column {samples.name} is not numeric.")
+    
+    # Calculate the mean of the attribute column
+    bulk_mean = np.mean(particle_data[attribute_column])
+    if np.isnan(bulk_mean):
+        warnings.warn("Bulk mean is NaN. Returning NaN for homogeneity index.")
+        return particle_data, np.nan
+    
     if homogeniety_column is None:
         homogeniety_column = attribute_column + "_homogeneity"
 
-    # Get the unique mesh ids
-    mesh = particle_data[sample_column]
-    mesh_ids = np.unique(mesh)
-    mesh_ids = mesh_ids[~np.isnan(mesh_ids)].astype(int)
-
-    mesh_data = []
     particle_homogeneity_data = np.empty(particle_data.n_points)
     particle_homogeneity_data[:] = np.nan
-    for mesh_id in mesh_ids:
-        mesh_boolean_mask = mesh == mesh_id
-        if not mesh_boolean_mask.any():
-            continue
 
+    sample_means = np.zeros(samples.n_occupied_cells)
+    for i, ids in enumerate(samples.occupied_cells):
+        sample_boolean_mask = particle_data[samples.name] == ids
+
+        # Calculate the mean of the attribute column for the mesh element
         mean = np.mean(
-             particle_data[attribute_column][mesh_boolean_mask.astype(bool)])
+             particle_data[attribute_column][sample_boolean_mask.astype(bool)])
+        sample_means[i] = mean
         
-        particle_homogeneity_data[mesh_boolean_mask] = mean
-        mesh_data.append([mesh_id, mean])
+        # Update the particle homogeneity data with the mean value
+        particle_homogeneity_data[sample_boolean_mask] = mean
 
     particle_data[homogeniety_column] = particle_homogeneity_data
-    mesh_homogeneity_df = pd.DataFrame(mesh_data, columns=["mesh id", "mean"])
 
-    return particle_data, mesh_homogeneity_df
+    # Calculate the homogeneity index
+    homogeneity_index = (sum((sample_means - bulk_mean) ** 2)
+                         / len(sample_means)) ** 0.5
+    
+    if verbose:
+        print("Homogeneity index: ", homogeneity_index)
+        print("Bulk mean: ", bulk_mean)
+        print("Sample means: ", sample_means)
+        print("Number of samples: ", len(sample_means))
+
+    return particle_data, homogeneity_index
 
 
         
--- examples/segregation.py
+++ examples/segregation.py
@@ -4,58 +4,67 @@ from natsort import natsorted
 from tqdm import tqdm
 import re
 import os
+import pandas as pd
 
-from DEMToolbox.particle_sampling import mesh_particles_1d
-from DEMToolbox.mixing import mean_mesh_element_radii
+from DEMToolbox.particle_sampling import sample_1d
+from DEMToolbox.mixing import homogeneity_index
 
 # Mesh 1D
 vector_1d = [0, 0, 1]
 resolution_1d = 50
 mesh_column_1d = "1D_mesh"
 
+# Simulation parameters
+timestep = 1e-5
+dumpstep = 0.1
+settled_time = 2
+
 # Save directories
 file_path = os.path.dirname(__file__)
 save_dir = os.path.join(file_path, "segregation_analysis")
 vtk_dir = os.path.join(save_dir, "post")
-csv_dir = os.path.join(save_dir, "csvs")
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 if not os.path.exists(vtk_dir):
     os.makedirs(vtk_dir)
-if not os.path.exists(csv_dir):
-    os.makedirs(csv_dir)
 
 # Get the particle files
 glob_input = os.path.join(file_path, "post", "particles_*")
 files = natsorted([f for f in glob.glob(glob_input) if "boundingBox" not in f])
 
+results = []
 for i, particle_file in enumerate(tqdm(files)):
 
     # Retrieve corresponding cylinder file for each particle file
     file_name_id = re.search(r'particles_(.*).vtk', particle_file).group(1)
     cylinder_name = "mesh_" + file_name_id + '.vtk'
+    time = float(file_name_id) * timestep
     cylinder_file = os.path.join(os.path.dirname(particle_file), cylinder_name)
-    
+
     # Read the particle and cylinder files
     particle_data = pv.read(particle_file)
     cylinder_data = pv.read(cylinder_file)
 
     # Mesh the particles
-    particle_data, mesh = mesh_particles_1d(particle_data, 
-                                                   cylinder_data, 
-                                                   vector_1d, 
-                                                   resolution_1d, 
-                                                   mesh_column_1d)
+    particle_data, samples = sample_1d(particle_data, 
+                                       cylinder_data, 
+                                       vector_1d, 
+                                       resolution_1d, 
+                                       mesh_column_1d)
+        
+    particle_data, hom = homogeneity_index(particle_data,
+                                           'radius',
+                                           samples, 
+                                           verbose=True)
     
-    particle_data, mesh_mean_radii_df = mean_mesh_element_radii(particle_data, 
-                                                                mesh.name, 
-                                                                mesh.mesh_df)
-    
-    save_csv = os.path.join(csv_dir, f"mesh_{file_name_id}.csv")
-    mesh_mean_radii_df.to_csv(save_csv, index=False)
-
+    # Save the particle data
     save_vtk = os.path.join(vtk_dir, os.path.basename(particle_file))
     particle_data.save(
         os.path.join(vtk_dir, 
                      ("mean_radii_" + os.path.basename(particle_file)))
         )
+    
+    results.append([time, hom])
+
+results_df = pd.DataFrame(results, columns=["time", "homogeneity"])
+results_df.to_csv(os.path.join(save_dir, "segregation_results.csv"), index=False)
